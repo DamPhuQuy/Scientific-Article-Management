a97c84b869edc12f273376e785ca9efc314e8a49
Dam Phu Quy <phuquydam06@gmail.com>
2025-10-24 22:06:02 +0700

    [UPDATE] supported .csv

--- src/main.cpp
+++ src/main.cpp
@@ -5,71 +5,21 @@
 using namespace std;
 namespace fs = std::filesystem; 
 
-void begin(){ 
-    cout<<"Chao mung den voi he thong quan ly bai bao"<<endl;
-    cout<<"-----------------------------------------------"<<endl;
-    cout<<"1. Quan ly bai bao"<<endl;
-    cout<<"2. Quan ly tac gia"<<endl;
-    cout<<"3. Quan ly tap chi"<<endl;
-    cout<<"4. Thoat"<<endl;
-    cout<<"-----------------------------------------------"<<endl;
-    cout<<"Hay chon mot lua chon (1-4): ";
-    int choice;
-    cin>>choice;
-    switch(choice){
-        case 1:
-            cout<<" Quan ly bai bao"<<endl;
-            cout<<"----------------------------/n";
-            Article ar;
-            string find;
-            cin.ignore;
-            cout<<"Nhap ten bai bao can tim:  ";
-            getline(cin,find);
-            
-            findArticleByRegex(article,articles_container,find,file_path,ar_repo,au_ar,ar_ref);
-        
-            break;
-        case 2:
-            cout<<" Quan ly tac gia"<<endl;
-            cout<<"----------------------------/n";
-            Author au;
-            string find;
-            cin.ignore;
-            cout<<"Nhap ten tac gia can tim:  ";
-            getline(cin,find);
-
-            findAuthorByRegex(author, au_con, find, file_path, au_repo, au_ar);
-            
-            break;
-        case 3:
-            cout<<" Quan ly tap chi"<<endl;
-            break;
-        case 4:
-            cout<<"Thoat!"<<endl;
-            exit(0);
-        default:
-            cout<<"hay thu lai."<<endl;
-            begin();
-    }
-}
-
 void start() {
+    // fetch data
     DataManipulation data_m; 
-
     ArticleRepo a_repo; 
     AuthorRepo au_repo; 
     ArticleReferenceRepo ar_ref; 
     AuthorArticleRepo au_ar; 
-
     data_m.fetchArticleDataSet(Constants::DataSet, a_repo, au_ar, ar_ref); 
     data_m.fetchAuthorInformation(Constants::AuthorInfo, au_repo); 
-
     RepositoryManager repo(a_repo, au_repo, au_ar, ar_ref);
+
+    repo.getArticles().traverse(); 
 }
 
 int main(){
     start();
-
-    begin();
     return 0;
 }
--- src/services/DataManipulation.cpp
+++ src/services/DataManipulation.cpp
@@ -4,7 +4,7 @@
 #include <sstream>
 #include <vector>
 #include <cctype>
-#include <stack> 
+#include <iomanip> // std::quoted
 #include <nlohmann/json.hpp>
 #include "DataManipulation.h"
 #include "CONFERENCE_Article.h"
@@ -41,10 +41,15 @@ bool DataManipulation::fileCheck(const fs::path &file_path, ifstream &in) {
         return false;
     }
 
-    if (file_path.extension() != ".json") {
-        cout << "ERROR: Invalid file extension (expected.json): " << file_path << "\n";
+    bool isCSV  = (file_path.extension() == ".csv");
+    bool isJSON = (file_path.extension() == ".json");
+
+    if (!isCSV && !isJSON) {
+        std::cout << "ERROR: Invalid file extension (expected .json or .csv): "
+                  << file_path << "\n";
         return false;
     }
+
 
     in.open(file_path, std::ios::in);
     if (!in.is_open()) {
@@ -70,26 +75,111 @@ void DataManipulation::fetchArticleDataSet(
     if (!fileCheck(file_path, in)) 
         return; 
 
-    json data;
-    in >> data;
+    if (file_path.extension() == ".json") {
+        json data;
+        in >> data;
 
-    for (auto& item : data) {
-        for (auto element : item["authors"]) {
-            au_ar.add(item["id"], element); 
-        }
-        
-        for (auto element : item["references"]) {
-            ar_ref.addReference(item["id"], element);  
-        }
+        for (auto& item : data) {
+            for (auto element : item["authors"]) {
+                au_ar.add(item["id"], element); 
+            }
 
-        ar_repo.add(createArticle(item["abstract"],
-                                  item["n_citation"],
-                                  item["title"],
-                                  item["venue"],
-                                  item["year"],
-                                  item["id"],
-                                  static_cast<Type>(item["type"]))); 
+            for (auto element : item["references"]) {
+                ar_ref.addReference(item["id"], element);  
+            }
+
+            ar_repo.add(createArticle(item["abstract"],
+                                      item["n_citation"],
+                                      item["title"],
+                                      item["venue"],
+                                      item["year"],
+                                      item["id"],
+                                      static_cast<Type>(item["type"]))); 
+        } 
     } 
+    else if (file_path.extension() == ".csv") {
+        string line; 
+        while (getline(in, line)) {
+            stringstream ss(line); 
+            vector<string> data; 
+            string field; 
+            while(ss.good()) {
+                // neu bat dau la "
+                if (ss.peek() == '"') {
+                    ss >> quoted(field); // doc het cho toi khi gap lai dau "
+                }
+                else {
+                    getline(ss, field, ','); 
+                }
+                data.push_back(field);
+                if (ss.peek() == ',') ss.ignore(); // bo qua dau phay
+            }
+
+            string abstract; 
+            vector<string> authors; 
+            int n_citation = 0; 
+            vector<string> refs; 
+            string title;
+            string venue;
+            int year = 0; 
+            string id;
+            int t = 0; 
+            Type type; 
+
+            for (int i = 0; i < data.size(); i++) {
+                string value = data[i]; 
+                switch(i) {
+                    case 0: // abstract
+                        abstract = value;
+                        break; 
+                    case 1: {// authors
+                        stringstream ss_au(value); 
+                        string token;
+                        while (getline(ss_au, token, ',')) {
+                            authors.push_back(token);
+                        }
+                        break; 
+                    }
+                    case 2: // n_citation
+                        n_citation = stoi(value);
+                        break;
+                    case 3: { // refs
+                        stringstream ss_refs(value); 
+                        string token; 
+                        while(getline(ss_refs, token, ',')) {
+                            refs.push_back(token); 
+                        }
+                        break; 
+                    }
+                    case 4: // title
+                        title = value; 
+                        break; 
+                    case 5: // venue
+                        venue = value; 
+                        break; 
+                    case 6: // year
+                        year = stoi(value); 
+                        break; 
+                    case 7: // id
+                        id = value; 
+                        break; 
+                    case 8: // type
+                        t = stoi(value); 
+                        type = static_cast<Type>(t);
+                }
+            }
+
+            for (auto element : authors) {
+                au_ar.add(id, element); 
+            }
+
+            for (auto element : refs) {
+                ar_ref.addReference(id, element); 
+            }
+
+            ar_repo.add(createArticle(abstract, n_citation, title, venue, year, id, type)); 
+        }
+    }
 }
 
 void DataManipulation::fetchAuthorInformation(const fs::path& file_path, AuthorRepo& au_repo) {
@@ -97,13 +187,61 @@ void DataManipulation::fetchAuthorInformation(const fs::path& file_path, AuthorR
     if (!fileCheck(file_path, in))
         return;
 
-    json data = json::parse(in); 
+    if (file_path.extension() == ".json") { 
+        json data = json::parse(in); 
+    
+        for (auto& item : data) {
+            au_repo.add(Author(item["id"],
+                         item["fullName"], 
+                         item["country"],
+                         item["fieldOfStudy"],
+                         item["totalPublications"])); 
+        } 
+    }
+    else if (file_path.extension() == ".csv") { 
+        string line; 
+        while (getline(in, line)) {
+            stringstream ss(line); 
+            vector<string> data; 
+            string field; 
+            while(ss.good()) {
+                // neu bat dau la "
+                if (ss.peek() == '"') {
+                    ss >> quoted(field); // doc het cho toi khi gap lai dau "
+                }
+                else {
+                    getline(ss, field, ','); 
+                }
+                data.push_back(field);
+                if (ss.peek() == ',') ss.ignore(); // bo qua dau phay
+            }
 
-    for (auto& item : data) {
-        au_repo.add(Author(item["id"],
-                     item["fullName"], 
-                     item["country"],
-                     item["fieldOfStudy"],
-                     item["totalPublications"])); 
-    } 
+            string id; 
+            string name; 
+            string country; 
+            string fieldOfStudy; 
+            int pub = 0; 
+
+            for (int i = 0; i < data.size(); i++) {
+                string value = data[i];
+                switch(i) {
+                    case 0: 
+                        id = value; 
+                        break; 
+                    case 1: 
+                        name = value; 
+                        break; 
+                    case 2: 
+                        country = value; 
+                        break; 
+                    case 3: 
+                        fieldOfStudy = value; 
+                    case 4: 
+                        pub = stoi(value);
+                }
+            }
+
+            au_repo.add(Author(id, name, country, fieldOfStudy, pub));
+        }
+    }
 }
